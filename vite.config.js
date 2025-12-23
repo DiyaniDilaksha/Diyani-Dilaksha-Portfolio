5d7d888a88a6eabd1b8f89a5df8bf9af9bfbf0e0
DiyaniDilaksha <diyanidilaksha@gmail.com>
2025-12-23 17:22:19 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Diyani-Dilaksha-Portfolio/',  
   plugins: [react()],
-   base: '/Diyani-Dilaksha-Portfolio/',  
+   
 })
